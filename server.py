3231b951367fdfc04c63c045da48185f2b86e925
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-10-19 16:42:31 +0300

    Create server.py

--- server.py
+++ server.py
@@ -1,50 +1,34 @@
-import socket
-import threading
-from algorithms import Algorithms
-from caesar_cipher import CaesarCipher
+import json
+
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+clients = []
 
 
-class ChatServer:
-    def __init__(self, host="127.0.0.1", port=12345, algorithm: Algorithms = None):
-        self.host = host
-        self.port = port
-        self.algorithm = algorithm
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.conn = None
-        self.addr = None
-
-    def start(self):
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen()
-        print(f"Server başlatıldı. Client bekleniyor")
-        self.conn, self.addr = self.server_socket.accept()
-        print(f"Bağlanan: {self.addr}")
-
-        threading.Thread(target=self.receive_messages, daemon=True).start()
-        threading.Thread(target=self.send_messages, daemon=True).start()
+@app.websocket("/ws/chat")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
         while True:
-            pass
+            data = await websocket.receive_text()
+            print(data)
+            for client in clients:
+                if client != websocket:
+                    await client.send_text(data)
 
-    def receive_messages(self):
-        while True:
-            try:
-                encrypted_data = self.conn.recv(1024).decode()
-                if not encrypted_data:
-                    break
-                print(f"\nEncrypted Message: {encrypted_data}")
-                print(f"Decrypted Message: {self.algorithm.decrypt(encrypted_data)}")
-            except Exception as e:
-                print(e)
-                break
-
-    def send_messages(self):
-        while True:
-            msg = input("")
-            encrypted_msg = self.algorithm.encrypt(msg)
-            self.conn.send(encrypted_msg.encode())
-
-
-if __name__ == "__main__":
-    caesar: Algorithms = CaesarCipher(shift=3)
-    server = ChatServer(algorithm=caesar)
-    server.start()
+    except Exception as e:
+        print("Client disconnected:", e)
+    finally:
+        clients.remove(websocket)
