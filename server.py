b503c97afd1d242a80763ff988c60a41611d1ccd
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-12-26 13:47:21 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-import json
-
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+import base64
 
 app = FastAPI()
+
+key = RSA.generate(2048)
+private_key = key
+public_key = key.publickey().export_key().decode()
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,14 +24,30 @@ clients = []
 @app.websocket("/ws/chat")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+
+    await websocket.send_json({
+        "type": "handshake",
+        "public_key": public_key
+    })
+
     clients.append(websocket)
+    session_aes_key = None
+
     try:
         while True:
-            data = await websocket.receive_text()
-            print(data)
+            message = await websocket.receive_json()
+
+            if message.get("type") == "key_exchange":
+                encrypted_aes_key = base64.b64decode(message["encrypted_key"])
+                cipher_rsa = PKCS1_v1_5.new(private_key)
+                sentinel = b'error'
+                session_aes_key = cipher_rsa.decrypt(encrypted_aes_key, sentinel)
+                print(f"AES Anahtarı başarıyla çözüldü: {session_aes_key}")
+                continue
+
             for client in clients:
                 if client != websocket:
-                    await client.send_text(data)
+                    await client.send_json(message)
 
     except Exception as e:
         print("Client disconnected:", e)
